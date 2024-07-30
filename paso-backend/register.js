41367b749b67be0cfee27052a525cb93ff02dbc4
Miguel Rumbo <86128109+MiguelRumbo@users.noreply.github.com>
2024-07-30 14:36:52 -0600

    Actualizacion Registro, Viaje, Perfil, Dashboard

--- paso-backend/register.js
+++ paso-backend/register.js
@@ -1,63 +1,119 @@
-// Guardar correo en localStorage y redirigir
-function saveEmail() {
-    const email = document.getElementById('email').value;
-    localStorage.setItem('email', email);
-    window.location.href = 'register_password.html';
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const emailForm = document.getElementById('email-form');
+    const passwordForm = document.getElementById('password-form');
+    const profileForm = document.getElementById('profile-form');
 
-// Guardar contraseña en localStorage y redirigir
-function savePassword() {
-    const password = document.getElementById('password').value;
-    const confirmPassword = document.getElementById('confirm-password').value;
+    // Función para mostrar el formulario de contraseña
+    function showPasswordForm() {
+        const email = document.getElementById('email').value;
+        const emailErrorMessage = document.getElementById('email-error-message');
 
-    if (password === confirmPassword) {
-        localStorage.setItem('password', password);
-        window.location.href = 'register_profile.html';
-    } else {
-        document.getElementById('error-message').style.display = 'block';
-    }
-}
-
-// Function to handle form submission
-async function submitProfile(event) {
-    event.preventDefault();
-    
-    const form = event.target;
-    const data = {
-        email: localStorage.getItem('email'), // Assuming email is stored in localStorage
-        password: localStorage.getItem('password'), // Assuming password is stored in localStorage
-        full_name: form.querySelector('#full-name').value,
-        age: form.querySelector('#age').value,
-        birthdate: form.querySelector('#birthdate').value,
-        gender: form.querySelector('#gender').value
-    };
-
-    try {
-        const response = await fetch('https://paso-app.ticsevn.com/register.php', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        });
-
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-
-        const result = await response.json();
-        console.log(result);
-
-        if (result.error) {
-            alert(`Error: ${result.error}`);
+        if (email.trim() === '') {
+            emailErrorMessage.style.display = 'block';
+            return;
         } else {
-            alert(result.message);
-            window.location.href = 'login.html';
+            emailErrorMessage.style.display = 'none';
+            emailForm.classList.add('hidden');
+            passwordForm.classList.remove('hidden');
         }
-    } catch (error) {
-        console.error('Error:', error);
     }
-}
 
-// Add event listener to the form
-document.getElementById('registration-form').addEventListener('submit', submitProfile);
+    // Función para mostrar el formulario de perfil
+    function showProfileForm() {
+        const password = document.getElementById('password').value;
+        const confirmPassword = document.getElementById('confirm-password').value;
+        const passwordErrorMessage = document.getElementById('password-error-message');
+        const errorMessage = document.getElementById('error-message');
+
+        if (password.trim() === '' || confirmPassword.trim() === '') {
+            passwordErrorMessage.style.display = 'block';
+            return;
+        } else {
+            passwordErrorMessage.style.display = 'none';
+            validatePassword();
+            passwordForm.classList.add('hidden');
+            profileForm.classList.remove('hidden');
+        }
+    }
+
+    // Función para validar la contraseña
+    function validatePassword() {
+        const password = document.getElementById('password').value;
+        const requirements = {
+            uppercase: /[A-Z]/.test(password),
+            special: /[!@#$%^&*(),.?":{}|<>]/.test(password),
+            number: /\d/.test(password),
+            length: password.length > 8,
+        };
+
+        for (const [key, isValid] of Object.entries(requirements)) {
+            const requirementElement = document.getElementById(key);
+            requirementElement.className = isValid ? 'valid' : 'invalid';
+        }
+
+        validateConfirmPassword();
+    }
+
+    // Función para validar la confirmación de la contraseña
+    function validateConfirmPassword() {
+        const password = document.getElementById('password').value;
+        const confirmPassword = document.getElementById('confirm-password').value;
+        const errorMessage = document.getElementById('error-message');
+
+        if (password !== confirmPassword) {
+            errorMessage.style.display = 'block';
+        } else {
+            errorMessage.style.display = 'none';
+        }
+    }
+
+    // Función para manejar la presentación del perfil
+    async function submitProfile(event) {
+        event.preventDefault();
+        
+        const data = {
+            email: document.getElementById('email').value,
+            password: document.getElementById('password').value,
+            first_name: document.getElementById('first-name').value,
+            last_name: document.getElementById('last-name').value,
+            mother_last_name: document.getElementById('mother-last-name').value,
+            birthdate: document.getElementById('birthdate').value,
+            gender: document.getElementById('gender').value
+        };
+
+        try {
+            const response = await fetch('https://paso-app.ticsevn.com/register.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const result = await response.json();
+            console.log(result);
+
+            if (result.error) {
+                alert(`Error: ${result.error}`);
+            } else {
+                alert(result.message);
+                window.location.href = 'login.html';
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+    // Agregar eventos a los botones
+    document.querySelector('.submit-button').addEventListener('click', showPasswordForm);
+    document.querySelector('.submit-button').addEventListener('click', showProfileForm);
+    document.getElementById('profile-form').addEventListener('submit', submitProfile);
+
+    // Eventos para validar las contraseñas
+    document.getElementById('password').addEventListener('input', validatePassword);
+    document.getElementById('confirm-password').addEventListener('input', validateConfirmPassword);
+});
